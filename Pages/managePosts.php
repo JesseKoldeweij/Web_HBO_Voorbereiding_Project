b93a8b920b629f72bf193620a705002cb71ad6d8
TimFlier123 <t.flier@student.alfa-college.nl>
2020-11-26 10:41:43 +0100

    managePosts.php

--- Pages/managePosts.php
+++ Pages/managePosts.php
@@ -1,29 +1,109 @@
 <?php
-if (!isAdmin()) {
-    header('location: index.php?Pages=login');
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['user']);
+    header("location: index.php?Pages=login");
 }
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'voorbereidinghbo');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($link === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 70%;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
 </head>
 <body>
-<h1>Ingelogd</h1>
-<br>
-<a href="index.php?logout" style="color: red;">Uitloggen</a>
-<br>
-<form method="POST" action="" id="" enctype="multipart/form-data">
-    <label>Titel: </label><br>
-    <input type="text" id="title" name="title"><br>
-    <label>Bericht: </label><br>
-    <textarea rows="4" cols="30" id="content" name="content"></textarea><br>
-    <label>Afbeelding toevoegen: </label><br>
-    <input type="file" name="image"><br>
-    <input type="submit" value="submit" name="submit">
-</form>
-<div id="content">
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header clearfix">
+                    <h2 class="pull-left">Employees Details</h2>
+                    <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+                </div>
+                <?php
+                // Include config file
+                require_once "Dbconfig.php";
+
+                // Attempt select query execution
+                $sql = "SELECT * FROM post";
+                if($result = mysqli_query($link, $sql)){
+                    if(mysqli_num_rows($result) > 0){
+                        echo "<table class='table table-bordered table-striped'>";
+                        echo "<thead>";
+                        echo "<tr>";
+                        echo "<th>id</th>";
+                        echo "<th>title</th>";
+                        echo "<th>content</th>";
+                        echo "<th>created at</th>";
+                        echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody>";
+                        while($row = mysqli_fetch_array($result)){
+                            echo "<tr>";
+                            echo "<td>" . $row['ID'] . "</td>";
+                            echo "<td>" . $row['title'] . "</td>";
+                            echo "<td>" . $row['content'] . "</td>";
+                            echo "<td>" . $row['created_at'] . "</td>";
+                            echo "<td>";
+                            echo "<a href='read.php?id=". $row['ID'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                            echo "<a href='update.php?id=". $row['ID'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                            echo "<a href='delete.php?id=". $row['ID'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";
+                        echo "</table>";
+                        // Free result set
+                        mysqli_free_result($result);
+                    } else{
+                        echo "<p class='lead'><em>No records were found.</em></p>";
+                    }
+                } else{
+                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                }
+
+                // Close connection
+                mysqli_close($link);
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
