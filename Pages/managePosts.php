fd96b99a56a3890d533e31b107d23b5f666ba0c2
TimFlier123 <t.flier@student.alfa-college.nl>
2020-11-26 10:14:38 +0100

    includes map gemaakt, header.php aangemaakt

--- Pages/managePosts.php
+++ Pages/managePosts.php
@@ -1,1 +1,29 @@
 <?php
+if (!isAdmin()) {
+    header('location: index.php?Pages=login');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1>Ingelogd</h1>
+<br>
+<a href="index.php?logout" style="color: red;">Uitloggen</a>
+<br>
+<form method="POST" action="" id="" enctype="multipart/form-data">
+    <label>Titel: </label><br>
+    <input type="text" id="title" name="title"><br>
+    <label>Bericht: </label><br>
+    <textarea rows="4" cols="30" id="content" name="content"></textarea><br>
+    <label>Afbeelding toevoegen: </label><br>
+    <input type="file" name="image"><br>
+    <input type="submit" value="submit" name="submit">
+</form>
+<div id="content">
+</div>
+</body>
+</html>
